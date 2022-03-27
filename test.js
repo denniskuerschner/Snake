58673ab42c84ce2b058ea9332196d9e2f50deaa4
Dennis <dennesch@gmail.com>
2022-03-27 09:31:57 +0200

    Test

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
-var text1;
+var points = '100 Punkte';
+//var username;
 
-document.getElementById('text1').innerHTML = text1;
+
+/*function usernameInput() {
+    username = document.getElementById("text1").value;
+        document.write("Peter Pan: ");
+        document.write(points);
+        document.write(username); */
+
+
+//document.getElementById('text1').innerHTML = text1;
+
+//Save username in txt-database and pull it from there?
